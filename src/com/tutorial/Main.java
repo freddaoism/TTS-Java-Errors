962984b3990451459ac1514d41089ede2678ddad
Fred Dao <freddaoism@gmail.com>
2021-05-06 13:06:57 -0400

    Initial commit

---
+++ src/com/tutorial/Main.java
@@ -0,0 +1,59 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int yearsTillDoomsday = 10;
+        //We are defining our variable (integer) on the line above
+
+        doubl oddsOfSurvival = 7.98;
+        // Doubles are variables with two decimal places
+
+        boolean ansOne  true;
+        // Booleans take true or false values
+
+        char meaningOfLife = 'G;
+        //Char holds single character values
+
+        String godPeople = "Flying Spaghetti Monster"
+        //String contains phrases and quotes
+
+        System.out.println("I am about to tell you a secret that will change your life just give me three seconds.");
+        //White space is used to make the code more readable to others
+
+        System.out.print("Three...");
+        //Example: Terminal -> javac whales.java -> executes ->
+
+        System.out.rint("Two...");
+        //The main method executes the tasks of the class
+
+        System.outprintln("One...");
+
+        System.out.println("We have " + yearsTillDoomsday + " years until Doomsday." );
+
+        System.out.println("We have a "  oddsOfSurvival + " percent chance of survival if we do everything right so listen to what I have to say.");
+
+        System.out.println("Hello Fred!);
+
+        System.out.println("You were born in 1996.");
+        /Sun Microsystems announced the release of Java in 1995 (this language is older than the guy using it!)
+
+        System.out.println("You were created by an alien race that was trying to preserve their species as their planet collapsed on itself.");
+        // Alien race is actually code for "first generation immigrant parents" and planet is actually code for "foreign country (Vietnam)"
+
+        System.out.println("You are here to save this planet from itself by creating innovative software that will prevent the same thing that happened to our planet. This is the beginning of your journey. Take it seriously.");
+        /* Save the world is code for "afford a mortgage" and innovative software is code for for "pay off crippling student debt" */
+
+        System.out.println("Are you ready?");
+
+        System.out.println(ansOne);
+        // Boolean action
+
+        System.out.println("Let's get started.");
+
+        System.out.println("By the way we figured out the meaning of life on our old planet..." + " it is " + meaningOfLife + " and that is why we must preserve this world.");
+
+        System.out.println("We also met the creator of this universe on the way out...and those " + godPeople + " people were actually right this whole time... who would've guessed?");
+    }
+}
